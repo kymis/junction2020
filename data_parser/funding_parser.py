bb4baaa9173964115dbc2e29adab1dd12aeb3621
Aada Alastalo <aada.alastalo@aalto.fi>
2020-11-07 14:20:02 +0200

    funding_parser.py

---
+++ data_parser/funding_parser.py
@@ -0,0 +1,27 @@
+import json
+
+file = open('../Data/data.json')
+
+# dictionary
+data = json.load(file)
+
+final_data = []
+  
+for e in data["haku"]:
+    # basic details
+    d = {
+        "Funding_ID": e["id"],
+        "Content_name": e["content"]["name"]["fi"],
+        "Duration": e["content"]["duration"]["end"],
+        "Self_financing_percentage": e["content"]["self-financing-percentage"],
+
+    }
+
+    if "rahoitusalueet" in e["content"] and len(e["content"]["rahoitusalueet"]) > 0:
+        d["Funding_areas"] = e["content"]["rahoitusalueet"]
+    
+    if "total-grant-size" in e["content"]:
+        d["Total_grant_size"] = e["content"]["total-grant-size"]
+
+    print(d)
+    
