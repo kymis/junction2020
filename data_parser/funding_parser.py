f8daefbc8264dc2d32878a503bd3f55395b6fda2
Akseli Aho <akseli.aho@aalto.fi>
2020-11-07 14:25:48 +0200

    ready

--- data_parser/funding_parser.py
+++ data_parser/funding_parser.py
@@ -1,27 +1,29 @@
 import json
 
-file = open('../Data/data.json')
 
-# dictionary
-data = json.load(file)
+def parse_fundings():
 
-final_data = []
-  
-for e in data["haku"]:
-    # basic details
-    d = {
-        "Funding_ID": e["id"],
-        "Content_name": e["content"]["name"]["fi"],
-        "Duration": e["content"]["duration"]["end"],
-        "Self_financing_percentage": e["content"]["self-financing-percentage"],
+    file = open('../Data/data.json')
+    # dictionary
+    data = json.load(file)
 
-    }
+    final_data = []
 
-    if "rahoitusalueet" in e["content"] and len(e["content"]["rahoitusalueet"]) > 0:
-        d["Funding_areas"] = e["content"]["rahoitusalueet"]
-    
-    if "total-grant-size" in e["content"]:
-        d["Total_grant_size"] = e["content"]["total-grant-size"]
+    for e in data["haku"]:
+        # basic details
+        d = {
+            "Funding_ID": e["id"],
+            "Content_name": e["content"]["name"]["fi"],
+            "Duration": e["content"]["duration"]["end"],
+            "Self_financing_percentage": e["content"]["self-financing-percentage"],
 
-    print(d)
-    
+        }
+
+        if "rahoitusalueet" in e["content"] and len(e["content"]["rahoitusalueet"]) > 0:
+            d["Funding_areas"] = e["content"]["rahoitusalueet"]
+
+        if "total-grant-size" in e["content"]:
+            d["Total_grant_size"] = e["content"]["total-grant-size"]
+
+    final_json = json.dumps(final_data, indent=4)
+    return final_json
--- data_parser/main.py
+++ data_parser/main.py
@@ -3,8 +3,11 @@ import funding_parser as fp
 import project_parser as pp
 
 projects_json = pp.parse_projects()
+fundings_json = fp.parse_fundings()
 
-print(projects_json)
 
-with open("Processed_data/projects.json", "w") as outfile:
+with open("../Processed_data/projects.json", "w") as outfile:
     outfile.write(projects_json)
+
+with open("../Processed_data/fundings.json", "w") as outfile:
+    outfile.write(fundings_json)
