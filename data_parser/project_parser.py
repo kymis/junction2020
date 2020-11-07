3ed1cbd6ccda81127d4da4447b5b532b1c4b1f92
Akseli Aho <akseli.aho@aalto.fi>
2020-11-07 13:47:09 +0200

    created main

--- data_parser/project_parser.py
+++ data_parser/project_parser.py
@@ -1,61 +1,61 @@
 import json
 
-file = open('../Data/data.json')
 
-# dictionary
-data = json.load(file)
+def parse_projects():
+    file = open('../Data/data.json')
 
-final_data = []
+    # dictionary
+    data = json.load(file)
 
-for e in data["loppuselvitys"]:
-    # basic details
-    d = {
-        "Project_ID": e["haku_id"],
-        "Project_name": e["project_name"],
-        "Organization_name": e["organization_name"]
-    }
-    v = e["loppuselvitys_answers"]["value"]
-    for obj in v:
-        # costs
-        if obj["key"] == "personnel-costs-row.amount":
-            d["personnel-costs"] = obj["value"]
-        elif obj["key"] == "other-costs-row.amount":
-            d["other-costs"] = obj["value"]
-        elif obj["key"] == "steamship-costs-row.amount":
-            d["steamship-costs"] = obj["value"]
-        elif obj["key"] == "equipment-costs-row.amount":
-            d["equipment-costs"] = obj["value"]
-        elif obj["key"] == "material-costs-row.amount":
-            d["material-costs"] = obj["value"]
-        elif obj["key"] == "coordination-costs-row.amount":
-            d["coordination-costs"] = obj["value"]
-        elif obj["key"] == "rent-costs-row.amount":
-            d["rent-costs"] = obj["value"]
-        elif obj["key"] == "service-purchase-costs-row.amount":
-            d["service-purchase-costs"] = obj["value"]
+    final_data = []
 
-        # incomes
-        if obj["key"] == "project-incomes-row.amount":
-            d["project-incomes"] = obj["value"]
-        elif obj["key"] == "other-public-financing-income-row.amount":
-            d["other-public-financing-income"] = obj["value"]
-        elif obj["key"] == "private-financing-income-row.amount":
-            d["private-financing-income"] = obj["value"]
-        elif obj["key"] == "eu-programs-income-row.amount":
-            d["eu-programs-income"] = obj["value"]
+    for e in data["loppuselvitys"]:
+        # basic details
+        d = {
+            "Project_ID": e["haku_id"],
+            "Project_name": e["project_name"],
+            "Organization_name": e["organization_name"]
+        }
+        v = e["loppuselvitys_answers"]["value"]
+        for obj in v:
+            # costs
+            if obj["key"] == "personnel-costs-row.amount":
+                d["personnel-costs"] = obj["value"]
+            elif obj["key"] == "other-costs-row.amount":
+                d["other-costs"] = obj["value"]
+            elif obj["key"] == "steamship-costs-row.amount":
+                d["steamship-costs"] = obj["value"]
+            elif obj["key"] == "equipment-costs-row.amount":
+                d["equipment-costs"] = obj["value"]
+            elif obj["key"] == "material-costs-row.amount":
+                d["material-costs"] = obj["value"]
+            elif obj["key"] == "coordination-costs-row.amount":
+                d["coordination-costs"] = obj["value"]
+            elif obj["key"] == "rent-costs-row.amount":
+                d["rent-costs"] = obj["value"]
+            elif obj["key"] == "service-purchase-costs-row.amount":
+                d["service-purchase-costs"] = obj["value"]
 
-        # outcomes
-        elif obj["key"] == "project-outcomes":
-            outcome_values = obj["value"][0]["value"]
-            for dv in outcome_values:
-                if dv["key"] == "project-outcomes.project-outcomes-1.outcome-type":
-                    d["Project_outcomeType"] = dv["value"]
-                elif dv["key"] == "project-outcomes.project-outcomes-1.description":
-                    d["Project_outcome"] = dv["value"]
-    print(d)
-    final_data.append(d)
+            # incomes
+            if obj["key"] == "project-incomes-row.amount":
+                d["project-incomes"] = obj["value"]
+            elif obj["key"] == "other-public-financing-income-row.amount":
+                d["other-public-financing-income"] = obj["value"]
+            elif obj["key"] == "private-financing-income-row.amount":
+                d["private-financing-income"] = obj["value"]
+            elif obj["key"] == "eu-programs-income-row.amount":
+                d["eu-programs-income"] = obj["value"]
+
+            # outcomes
+            elif obj["key"] == "project-outcomes":
+                outcome_values = obj["value"][0]["value"]
+                for dv in outcome_values:
+                    if dv["key"] == "project-outcomes.project-outcomes-1.outcome-type":
+                        d["Project_outcomeType"] = dv["value"]
+                    elif dv["key"] == "project-outcomes.project-outcomes-1.description":
+                        d["Project_outcome"] = dv["value"]
+        print(d)
+        final_data.append(d)
 
     final_json = json.dumps(final_data, indent=4)
-    with open("../Processed_data/projects.json", "w") as outfile:
-        outfile.write(final_json)
-
+    return final_json
